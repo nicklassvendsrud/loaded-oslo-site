e345a8a18b826b45921950c1777f9cb9422f8c9c
Nicklas Svendsrud <nicklas.svendsrud@gmail.com>
2020-12-16 17:12:50 +0100

    Add FB pixel in head

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -14,9 +14,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang={this.props.lang || 'en'}>
         <Head>
-          <!-- Facebook Pixel Code -->
-          <script>
-            !function(f,b,e,v,n,t,s)
+          <script dangerouslySetInnerHTML={{__html: `!function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -24,14 +22,13 @@ export default class MyDocument extends Document {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '{your-pixel-id-goes-here}');
-            fbq('track', 'PageView');
-          </script>
+            fbq('init', '9fql9y2oj3h422rs2yjlmcd77f9glv');
+            fbq('track', 'PageView');`}}
+          />
           <noscript>
-            <img height="1" width="1" style="display:none" 
-                 src="https://www.facebook.com/tr?id=9fql9y2oj3h422rs2yjlmcd77f9glv&ev=PageView&noscript=1"/>
+            <img height='1' width='1' style='display:none'
+              src='https://www.facebook.com/tr?id=9fql9y2oj3h422rs2yjlmcd77f9glv&ev=PageView&noscript=1' />
           </noscript>
-          <!-- End Facebook Pixel Code -->
         </Head>
         <body>
           <Main />
